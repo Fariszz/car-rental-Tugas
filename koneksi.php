0194a40cc6b74cc899eb4b2c3b9633f68dc07844
GalihPrev <galihpreviand27@gmail.com>
2021-06-03 12:16:05 +0700

    Create koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php 
+    $namaHost = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "rental_mobil";
+
+    $connect = mysqli_connect($namaHost,$username,$password,$database);
+    
+?>
