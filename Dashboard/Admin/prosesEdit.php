7dc4bb50e4a475beaec778f4c4589b990a877941
Muhammad Faris Hadi Mulyo <ziraflyo@gmail.com>
2021-06-09 06:51:25 +0700

    Updated Admin

---
+++ Dashboard/Admin/prosesEdit.php
@@ -0,0 +1,18 @@
+<?php 
+    include "koneksi.php";
+
+    $id = $_GET['passAdmin'];  
+
+    $query = "UPDATE admin SET password_admin = '$id'";
+    $result = mysqli_query($connect, $query);
+
+    if ($result) {
+        header("location:/car-rental-Tugas/Dashboard/Admin/");
+        ?>
+    
+    <?php 
+        }else{
+            echo "Gagal update data ". mysqli_error($connect);
+        }
+
+?>
