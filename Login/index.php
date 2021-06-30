39183a2ba00c7042b46620e11865d279d8db4fb0
Muhammad Faris Hadi Mulyo <ziraflyo@gmail.com>
2021-06-30 12:43:27 +0700

    Update Route

--- Login/index.php
+++ Login/index.php
@@ -1,59 +1,59 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title></title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-  </head>
-  <body>
-<form action="LoginUser.php" method="POST" onsubmit="return validasi()">
 
- <div class="left-page">
-  <form action="loginMultiProses.php" method="POST">
+<head>
+  <title></title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
 
- <div class="login-kotak-kotak">
-  <font size="64px">Sign in</font><br><br><br>
-  <div class="kotakteks">
-    <input type="text" placeholder="Username " name="username" id="username">
+<body>
+  <form action="LoginUser.php" method="POST" onsubmit="return validasi()">
+
+    <div class="left-page">
+      <form action="loginMultiProses.php" method="POST">
+
+        <div class="login-kotak-kotak">
+          <font size="64px">Sign in</font><br><br><br>
+          <div class="kotakteks">
+            <input type="text" placeholder="Username " name="username" id="username">
+          </div>
+
+          <br><br>
+
+          <div class="kotakteks">
+            <input type="password" placeholder="Password" name="password" id="password">
+          </div>
+
+          <div class="masuk">
+            <input type="submit" class="Sigin" value="Sign in" name="Sigin" id="Sigin">
+          </div>
+
+          <div class="newacc">
+            <a href="../Register/index.php">Create New Account</a>
+          </div>
+        </div>
+    </div>
+  </form>
+  </form>
+
+  <div class="right-page">
+    <p><img src="gambar.svg" width="600" height="300" style="margin-left: px;"></p>
   </div>
 
-  <br><br>
-  
-  <div class="kotakteks">
-    <input type="password" placeholder="Password" name="password" id="password">
   </div>
-  
-  <div class="masuk">
-  <input type="submit" class="Sigin" value="Sign in" name="Sigin" id="Sigin" >
-  </div>
-  
-  <div class="newacc">
-  <a href="../Register/index.php">Create New Account</a>
-  </div>
-</div>
-</div>
-</form>
-</form>
 
- <div class="right-page">
-  <p><img src="gambar.svg" width="600"height ="300" style="margin-left: px;"></p>
- </div>
- 
-</div>
-
-  </body>
+</body>
 
 </html>
 
 <script>
-
-function validasi() {
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
-  if (username != "" && password !="") {
-    return true;
-  }else{
-    alert('Silahkan mengisi data dengan lengkap');
+  function validasi() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username != "" && password != "") {
+      return true;
+    } else {
+      alert('Silahkan mengisi data dengan lengkap');
+    }
   }
-}
-
 </script>
